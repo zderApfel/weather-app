f2bfc2052a098ed45d1478cd68c8120a6033e63e
zderApfel <scoutt172@yahoo.com>
2020-09-24 17:34:44 -0400

    Added temperature conversion buttons

--- script.js
+++ script.js
@@ -17,13 +17,14 @@ async function getWeather(){
         loadingArea.textContent = "Loading...";
         let dataFetch = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm.value}&appid=${CONFIG.API_KEY}`, {mode: 'cors'});
         let weather = await dataFetch.json();
-        weatherDOM(weather);
-    } catch(err){
-        displayError(err.message);
-    }
+        return weather;
+    }finally{}
 }
 
 function weatherDOM(data) {
+    let tempData = setTemp(data.main.temp);
+    let feelsData = setTemp(data.main.feels_like);
+
     const CITY_NAME = document.querySelector("#city");
     const WEATHER = document.querySelector("#weather");
     const TEMPERATURE = document.querySelector("#temperature");
@@ -31,13 +32,56 @@ function weatherDOM(data) {
     const FEELS_LIKE = document.querySelector("#feels-like");
     CITY_NAME.textContent = `${data.name}, ${data.sys.country}`;
     WEATHER.textContent = data.weather[0].main;
-    TEMPERATURE.textContent = `Temperature - ${kelvinToF(data.main.temp)}F`;
+    TEMPERATURE.textContent = `Temperature - ${tempData}F`;
     HUMIDITY.textContent = `Humidity - ${data.main.humidity}%`;
-    FEELS_LIKE.textContent = `(Feels Like ${kelvinToF(data.main.feels_like)}F)`;
+    FEELS_LIKE.textContent = `(Feels Like ${feelsData}F)`;
+
+    const BUTTON_HOLDER = document.querySelector("#button-holder");
+
+    const fareinheitToCelsius = document.createElement('button');
+    fareinheitToCelsius.id = 'f-to-c';
+    fareinheitToCelsius.className = 'conversion-button';
+    fareinheitToCelsius.onclick = () => {
+        tempData = fToC(tempData);
+        feelsData = fToC(feelsData);
+        TEMPERATURE.textContent = `Temperature - ${tempData}C`;
+        FEELS_LIKE.textContent = `(Feels Like ${feelsData}C)`;
+        fareinheitToCelsius.disabled = true;
+        celsiusToFareinheit.disabled = false;
+    };
+    fareinheitToCelsius.textContent = "To C";
+
+    const celsiusToFareinheit = document.createElement('button');
+    celsiusToFareinheit.id = 'c-to-f';
+    celsiusToFareinheit.className = 'conversion-button';
+    celsiusToFareinheit.onclick = () => {
+        tempData = cToF(tempData);
+        feelsData = cToF(feelsData);
+        TEMPERATURE.textContent = `Temperature - ${tempData}F`;
+        FEELS_LIKE.textContent = `(Feels Like ${feelsData}F)`;
+        celsiusToFareinheit.disabled = true;
+        fareinheitToCelsius.disabled = false;
+    };
+    celsiusToFareinheit.textContent = "To F";
+    celsiusToFareinheit.disabled = true;
+
+    BUTTON_HOLDER.innerHTML = "";
+    BUTTON_HOLDER.appendChild(fareinheitToCelsius);
+    BUTTON_HOLDER.appendChild(celsiusToFareinheit);
 }
 
-function kelvinToF(temperature){ //The API returns temperatures as Kelvin apparently
+function setTemp(temperature){ //The API returns temperatures as Kelvin apparently
     let temp = (temperature - 273.15) * (9/5) + 32;
+    return Math.round(temp);
+}
+
+function fToC(temperature){
+    let temp = (temperature - 32) * (5/9);
+    return Math.round(temp);
+}
+
+function cToF(temperature){
+    let temp = (temperature * 9/5) + 32;
     return Math.round(temp);
 }
 
