ed02cab52609ced90e2af3152fb33f6d67beea3d
zderApfel <scoutt172@yahoo.com>
2020-09-24 16:56:10 -0400

    Styled error handler

--- script.js
+++ script.js
@@ -10,16 +10,16 @@
     * Feels Like and Temperature values are in Kelvin and will need to be converted for Celcius and Fareinheit
 */
 
-async function getWeather(location){
+async function getWeather(){
     const searchTerm = document.querySelector("#search-term");
     const loadingArea = document.querySelector("#city");
     try{
+        loadingArea.textContent = "Loading...";
         let dataFetch = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm.value}&appid=${CONFIG.API_KEY}`, {mode: 'cors'});
         let weather = await dataFetch.json();
-        loadingArea.textContent = "Loading..."
-        return weather;
+        weatherDOM(weather);
     } catch(err){
-        console.log("Oops! " + err);
+        displayError(err.message);
     }
 }
 
@@ -29,9 +29,19 @@ function weatherDOM(data) {
     const TEMPERATURE = document.querySelector("#temperature");
     const HUMIDITY = document.querySelector("#humidity");
     const FEELS_LIKE = document.querySelector("#feels-like");
-    CITY_NAME.textContent = data.name;
+    CITY_NAME.textContent = `${data.name}, ${data.sys.country}`;
     WEATHER.textContent = data.weather[0].main;
-    TEMPERATURE.textContent = `Temperature ${data.main.temp}K`;
-    HUMIDITY.textContent = `Humidity: ${data.main.humidity}%`;
-    FEELS_LIKE.textContent = `(Feels Like: ${data.main.feels_like}K)`;
+    TEMPERATURE.textContent = `Temperature - ${kelvinToF(data.main.temp)}F`;
+    HUMIDITY.textContent = `Humidity - ${data.main.humidity}%`;
+    FEELS_LIKE.textContent = `(Feels Like ${kelvinToF(data.main.feels_like)}F)`;
+}
+
+function kelvinToF(temperature){ //The API returns temperatures as Kelvin apparently
+    let temp = (temperature - 273.15) * (9/5) + 32;
+    return Math.round(temp);
+}
+
+function displayError(){
+    const TEXT_DISPLAY = document.querySelector("#city");
+    TEXT_DISPLAY.textContent = "Oopsies, something went wrong! Check your city name and try again or refresh the page";
 }
